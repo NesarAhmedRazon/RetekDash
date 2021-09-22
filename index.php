c3b609ede3fbdac29439abbef96c04b2f1a93640
NesarAhmedRazon <grim.roy@gmail.com>
2021-09-22 18:32:16 +0600

    update on demo

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="/assets/style.css?v=<?php echo time();?>">
+    <link rel="stylesheet" href="assets/style.css?v=<?php echo time();?>">
 </head>
 
 <body class="bg-lucky text-white">
@@ -24,7 +24,7 @@
                 <div class="row fhead">
                     <div class="col-6">
                         <picture>
-                            <img src="/assets/itPrix-01-01.svg" alt="" class="w-25">
+                            <img src="assets/itPrix-01-01.svg" alt="" class="w-25">
                         </picture>
                     </div>
                     <div class="col-6">hem</div>
@@ -92,7 +92,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
         crossorigin="anonymous"></script>
-    <script src="/assets/app.js"></script>
+    <script src="assets/app.js"></script>
 </body>
 
 </html>
